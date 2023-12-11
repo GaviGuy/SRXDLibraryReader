275f0d1bf78a99a58be2566f85fd8332b1a72a67
GaviGuy <gaviguy711@gmail.com>
2023-12-11 02:15:39 -0700

    fixing dependency

--- Program.cs
+++ Program.cs
@@ -13,36 +13,29 @@ namespace LibraryReader {
 
             SRTB curr;
             int count = 0;
-            //bool ranged = false;
-            //int min = 0, max = 99
-            //if (args.Length > 0) {
-            //    min = Int32.Parse(args[0]);
-            //    ranged = true;
-            //}
-            //if (args.Length > 1)
-            //    max = Int32.Parse(args[1]);
+            bool ranged = false;
+            int min = 0, max = 99;
+            if (args.Length > 0) {
+                min = Int32.Parse(args[0]);
+                ranged = true;
+            }
+            if (args.Length > 1)
+                max = Int32.Parse(args[1]);
             for (int i = 0; i < sourcePaths.Length; i++) {
                 try {
                     curr = SRTB.DeserializeFromFile(sourcePaths[i]);
                     var trackInfo = curr.GetTrackInfo();
                     count++;
 
-                    //for (int j = 0; j < trackInfo.Difficulties.Count; j++) {
-                    //    if (trackInfo.Difficulties[j].Active) {
-                    //        var currDiff = curr.GetTrackData(j);
-                    //        if(ranged && currDiff.DifficultyRating >= min && currDiff.DifficultyRating <= max) {
+                    for (int j = 0; j < trackInfo.Difficulties.Count; j++) {
+                        if (trackInfo.Difficulties[j].Active) {
+                            var currDiff = curr.GetTrackData(j);
+                            if(!ranged || (currDiff.DifficultyRating >= min && currDiff.DifficultyRating <= max)) {
                                 output += trackInfo.Title + " - " + trackInfo.ArtistName + " - " + trackInfo.Charter + '\n';
-                            //switch (currDiff.DifficultyType) {
-                            //        case SRTB.DifficultyType.Easy: output += "Easy "; break;
-                            //        case SRTB.DifficultyType.Normal: output += "Normal "; break;
-                            //        case SRTB.DifficultyType.Hard: output += "Hard "; break;
-                            //        case SRTB.DifficultyType.Expert: output += "Expert "; break;
-                            //        case SRTB.DifficultyType.XD: output += "XD "; break;
-                            //        case (SRTB.DifficultyType)7: output += "RemiXD "; break;
-                            //    }
-                            //}
-                    //    }
-                    //}
+                                break;
+                            }
+                        }
+                    }
                 }
                 catch (Exception E) {
                     Console.Write("messed up with chart " + sourcePaths[i] + '\n');
